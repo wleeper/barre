43bf7eb8f28273d93dc6450e594a56140252db25
Bill Leeper <wleeper@gmail.com>
2015-02-16 12:15:28 -0700

    base profile, fixed bug in signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
+before_filter :configure_account_update_params, only: [:update]
 
   layout 'devise'
   
@@ -42,16 +42,21 @@ before_filter :configure_sign_up_params, only: [:create]
 
   # You can put the params you want to permit in the empty array.
   def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:password, :password_confirmation, :email, :first_name, :last_name) }
   end
 
   # You can put the params you want to permit in the empty array.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:password, :password_confirmation, :current_password, :email, :first_name, :last_name) }
+  end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
+    dashboard_path
+  end
+
+  # The path used after sign up.
+  def after_update_path_for(resource)
     dashboard_path
   end
 
